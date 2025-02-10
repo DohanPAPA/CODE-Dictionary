bd5bdd104635179f8f0758cf2a0c6f6b7fda0f76
KKH <kkh@avaco.com>
2025-02-10 14:35:52 +0900

    메서드 체이닝 기술 예제

---
+++ Code Dictionary_C#/Method Chaining.cs
@@ -0,0 +1,42 @@
+﻿namespace Code_Dictionary_C_
+{
+    // 메서드 체이닝 
+    // 여러 메소드 호출을 단일 명령문의 형태로 결합해서 일련의 작업을 수행할 수 있는 기술
+    internal class Method_Chaining
+    {
+        Calculator calc = new Calculator(5);
+
+        public Method_Chaining()
+        {
+            // 연쇄적으로 메서드 호출 가능
+            calc.Add(2).Subtract(2).Multiply(2);
+        }
+    }
+
+    public class Calculator
+    {
+        private int _value;
+
+        public Calculator(int initiaValue)
+        {
+            _value = initiaValue;
+        }
+
+        public Calculator Add(int number)
+        {
+            _value += number;
+            return this;
+        }
+        public Calculator Subtract(int number)
+        {
+            _value -= number;
+            return this;
+        }
+        public Calculator Multiply(int number)
+        {
+            _value *= number;
+            return this;
+        }
+    }
+
+}
